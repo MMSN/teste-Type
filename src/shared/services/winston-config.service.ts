fae5c799cd59ca5fff9ddce7b8e170b5a010f193
Mateus Nascimento <mateus.msnascimento@gmail.com>
2022-05-28 22:03:07 -0300

    fix: remove what is not necessary

--- src/shared/services/winston-config.service.ts
+++ src/shared/services/winston-config.service.ts
@@ -6,7 +6,6 @@ import {
 } from 'nest-winston';
 import * as winston from 'winston';
 import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
-import WinstonCloudwatch from 'winston-cloudwatch';
 import * as Transport from 'winston-transport';
 import { EnvironmentEnum } from '../enums/environment.enum';
 
@@ -19,7 +18,7 @@ export class WinstonConfigService implements WinstonModuleOptionsFactory {
     const level = this.configService.get<string>('LOG_LEVEL', 'debug');
     const nodeEnv = this.configService.get<EnvironmentEnum>('NODE_ENV');
     const appName = this.configService.get<string>('npm_package_name');
-    const logGroup = `housi/microservices/${nodeEnv}`;
+    const logGroup = `testeType/microservices/${nodeEnv}`;
 
     const transports: Transport[] = [
       new winston.transports.Console({
@@ -29,22 +28,6 @@ export class WinstonConfigService implements WinstonModuleOptionsFactory {
         ),
       }),
     ];
-
-    if ([PRODUCTION, SANDBOX, STAGING].includes(nodeEnv)) {
-      transports.push(
-        new WinstonCloudwatch({
-          jsonMessage: true,
-          awsAccessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
-          awsSecretKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
-          awsRegion: this.configService.get<string>('AWS_REGION', 'us-east-1'),
-          logGroupName: this.configService.get<string>('LOG_GROUP', logGroup),
-          logStreamName: this.configService.get<string>(
-            'LOG_STREAM_NAME',
-            appName,
-          ),
-        }),
-      );
-    }
 
     return { level, transports };
   }
--- src/shared/storage/storage.factory.ts
+++ src/shared/storage/storage.factory.ts
@@ -1,12 +1,9 @@
-import S3Storage from './s3-storage';
 import LocalStorage from './local-storage';
 import { StorageInterface } from './storage.interface';
 
 class StorageFactory {
   public static make(serviceName?: string): StorageInterface {
     switch (serviceName) {
-      case 's3':
-        return new S3Storage();
       case 'local':
         return new LocalStorage();
       default:
