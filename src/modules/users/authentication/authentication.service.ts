7e5761c87ca2157cf23637319ebfdf447dd134dd
Mateus Nascimento <mateus.msnascimento@gmail.com>
2022-06-05 16:19:53 -0300

    feat: add auth user method

--- src/modules/users/authentication/authentication.service.ts
+++ src/modules/users/authentication/authentication.service.ts
@@ -25,4 +25,25 @@ export class AuthenticationService {
       throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
+
+  public async getAuthenticatedUser (email: string, plainTextPassword: string) {
+    try {
+      const user = await this.usersService.getUserByEmail(email)
+      await this.verifyPassword(plainTextPassword, user.password);
+      delete user.password
+      return user;
+    } catch (error) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  private async verifyPassword(plainTextPassword: string, hashedPassword: string) {
+    const isPasswordMatching = await bcrypt.compare(
+      plainTextPassword,
+      hashedPassword
+    );
+    if (!isPasswordMatching) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+    }
+  }
 }
