b4b26c8dd51d4190767e37dd215588b037871b28
Mateus Nascimento <mateus.msnascimento@gmail.com>
2022-07-22 14:33:29 -0300

    feat: update controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,16 +1,18 @@
 import { Body, Controller, Post } from "@nestjs/common";
+import { AuthenticationService } from "./authentication/authentication.service";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UsersService } from "./services/users.service";
 
 @Controller('users')
 export default class UsersController {
   constructor(
-    private readonly usersService: UsersService
+    private readonly usersService: UsersService,
+    private readonly authenticationService: AuthenticationService
   ) {}
 
   @Post()
   async createPost(@Body() user: CreateUserDto) {
-    return this.usersService.create(user);
+    return this.authenticationService.registerUser(user)
   }
 
 }
