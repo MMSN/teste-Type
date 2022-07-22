89096289bb54761774f281c1981b3101518f81f7
Mateus Nascimento <mateus.msnascimento@gmail.com>
2022-07-22 16:04:24 -0300

    feat: set up passport and basic strategy

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
-import { UsersService } from "../users/services/users.service";
+import { PassportModule } from "@nestjs/passport";
 import { UsersModule } from "../users/users.module";
 import { AuthenticationService } from "./services/authentication.service";
+import { LocalStrategy } from "./strategy/local.strategy";
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule
   ],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, LocalStrategy],
   exports: [AuthenticationService]
 })
 export class AuthenticationModule {}
